c0dca73d6b117c50b48924aeeca7fb944bd85526
Matthew Lee <mattlee446@gmail.com>
2017-02-15 14:36:31 -0500

    X stuff

---
+++ xmonad.hs
@@ -0,0 +1,28 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , borderWidth = 1
+        , normalBorderColor = colorBlackAlt
+        , focusedBorderColor = colorWhiteAlt
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot ~/Pictures/Screenshots/%b%d::%H%M%S.png")
+        ]
+
+colorWhiteAlt = "#b5b3b3"
+colorBlackAlt = "#1c1c1c"
