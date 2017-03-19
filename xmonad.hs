6944d9ee5cbf415bb40d4fe35798ad93b9b6e624
kynelee <matt@kynelee.com>
2017-03-18 22:54:29 -0400

    xmonad within XFCE

--- xmonad.hs
+++ xmonad.hs
@@ -1,28 +1,17 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+import XMonad.Config.Xfce
+import XMonad.Util.EZConfig 
 
-main = do
-    xmproc <- spawnPipe "xmobar"
-
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , borderWidth = 1
-        , normalBorderColor = colorBlackAlt
-        , focusedBorderColor = colorWhiteAlt
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot ~/Pictures/Screenshots/%b%d::%H%M%S.png")
-        ]
+main = xmonad $ xfceConfig
+	{
+	  borderWidth = 1
+	, terminal = "xterm"
+	, normalBorderColor = colorBlackAlt
+	, focusedBorderColor = colorWhiteAlt
+	} `additionalKeys` myKeys
 
 colorWhiteAlt = "#b5b3b3"
 colorBlackAlt = "#1c1c1c"
+
+myLauncher = "rofi -show run -modi run, window -sidebar-mode -lines 6 -width 400"
+myKeys = [ ((mod1Mask, xK_p), spawn myLauncher) ]
