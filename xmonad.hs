042f38d926a1b3bdecbd6d2f3499f44808f18f59
kynelee <matt@kynelee.com>
2017-04-11 17:03:35 -0400

    rofi window switcher

--- xmonad.hs
+++ xmonad.hs
@@ -12,6 +12,5 @@ main = xmonad $ xfceConfig
 
 colorWhiteAlt = "#b5b3b3"
 colorBlackAlt = "#1c1c1c"
-
 myLauncher = "rofi -show run -modi run, window -sidebar-mode -lines 6 -width 400"
 myKeys = [ ((mod1Mask, xK_p), spawn myLauncher) ]
